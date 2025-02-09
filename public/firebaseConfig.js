6be149975cba712c7b32da5bb5e253e9208254cb
Mojgan Behravan <mojgan.behravan@gmail.com>
2025-02-09 22:07:51 +0100

    Removed firebaseConfig.js from Git tracking

--- public/firebaseConfig.js
+++
@@ -1,5 +0,0 @@
-export const firebaseConfig = {
-    apiKey: "AIzaSyCDBB8YJrLCbbJKVqPVeqJ-AHU_8ptMNLA",
-    authDomain: "food-ordering-81175.firebaseapp.com",
-    projectId: "food-ordering-81175",
-  };
