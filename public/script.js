9535e25aa458f145c7dcd51f412ac97917b879fd
Mojgan Behravan <mojgan.behravan@gmail.com>
2025-02-09 21:03:14 +0100

    remove apikey for security

--- public/script.js
+++ public/script.js
@@ -5,7 +5,7 @@ import { getFirestore, collection, getDocs } from "https://www.gstatic.com/fireb
 
 // Firebase Config
 const firebaseConfig = {
-  apiKey: "AIzaSyCDBB8YJrLCbbJKVqPVeqJ-AHU_8ptMNLA",
+  apiKey: "",
   authDomain: "food-ordering-81175.firebaseapp.com",
   projectId: "food-ordering-81175",
  
